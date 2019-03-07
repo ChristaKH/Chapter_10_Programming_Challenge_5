4779e752ffccb15692c06fd22f8fe6da54fc5b04
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-03-06 21:54:21 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * @author: Christa Hatch
+ * @Course: CS 151 - Advanced C++ Programming
+ * @Date: 03/06/19
+ *
+ * Problem Statement: Pie A La Mode - In statistics the mode of a set of values is the value that occurs
+ * the most often. Write a program that determines how many pieces of pie most people eat in a year.Set
+ * up an integer array that can hold responses from 30 people. For each person, enter the number of pieces
+ * they say they eat in the year. Then write a function that finds the mode of these 30 values. This will
+ * be the number of pie slices eaten by the most people. The function that finds and returns the mode should
+ * accept two arguments, an array of integers, and a value indicating how many elements are in the array.
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
